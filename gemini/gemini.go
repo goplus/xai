97b8174b0f199af67253878a8deb4dcc72279694
xushiwei <x@goplus.org>
2026-02-24 12:21:43 +0800

    provider: gemini

---
+++ gemini/gemini.go
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2026 The XGo Authors (xgo.dev). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gemini
+
+import (
+	"context"
+
+	"github.com/goplus/xai"
+	"google.golang.org/genai"
+)
+
+var (
+	_ xai.Provider = (*Provider)(nil)
+)
+
+// -----------------------------------------------------------------------------
+
+type Provider struct {
+	cli genai.Client
+}
+
+func (p *Provider) Chat(ctx context.Context, params xai.ParamBuilder, opts xai.OptionBuilder) (xai.Message, error) {
+	panic("todo")
+}
+
+func (p *Provider) ChatStreaming(ctx context.Context, params xai.ParamBuilder, opts xai.OptionBuilder) xai.StreamMessage {
+	panic("todo")
+}
+
+// -----------------------------------------------------------------------------
