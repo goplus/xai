5aaf33023a78429b465e545a74c1d481b37853d0
xushiwei <x@goplus.org>
2026-03-10 09:23:35 +0800

    geno.ServiceBase: Operation

--- spec/geno/geno.go
+++ spec/geno/geno.go
@@ -18,6 +18,7 @@ package geno
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 
 	xai "github.com/goplus/xai/spec"
@@ -25,44 +26,91 @@ import (
 
 // -----------------------------------------------------------------------------
 
-type ServiceBase struct {
+type ActionInfo struct {
+	Path           string   // path to call for the action, e.g. "/v1/images/generations"
+	ModelParamName string   // name of the parameter for the model, e.g. "model_name"
+	QueryPath      string   // path to query status for the action, e.g. "/v1/images/generations/{id}"
+	QueryID        []string // e.g. ["data", "task_id"] for resp["data"]["task_id"]
+}
+
+type adapter interface {
+	ActionInfo(action xai.Action) ActionInfo
+}
+
+// -----------------------------------------------------------------------------
+
+type ServiceBase[T adapter] struct {
 	c Client
 }
 
-func NewServiceBase(client *http.Client) *ServiceBase {
-	return &ServiceBase{
+// NewServiceBase creates a new ServiceBase with the provided HTTP client.
+func NewServiceBase[T adapter](client *http.Client) *ServiceBase[T] {
+	return &ServiceBase[T]{
 		c: *NewClient(client),
 	}
 }
 
-func (p *ServiceBase) SetOptions(opts *HTTPOptions) {
-	p.c.baseURL = opts.baseURL
-	if opts.timeout != nil {
-		p.c.Timeout(*opts.timeout)
-	}
+// HTTPClient returns the underlying HTTP client used by the service.
+func (p *ServiceBase[T]) HTTPClient() *Client {
+	return &p.c
 }
 
-func (p *ServiceBase) Options() xai.OptionBuilder {
+// to implement xai.Service
+func (p *ServiceBase[T]) Options() xai.OptionBuilder {
 	return new(HTTPOptions)
 }
 
-func (p *ServiceBase) NewOperation(method, path string) (ret Operation, err error) {
-	ret.req, err = p.c.NewRequest(method, path)
-	return
+// to implement xai.Service
+func (p *ServiceBase[T]) Operation(model xai.Model, action xai.Action) (xai.Operation, error) {
+	var adapter T
+	ai := adapter.ActionInfo(action)
+	req, err := p.c.NewRequest(http.MethodPost, ai.Path)
+	if err != nil {
+		return nil, err
+	}
+	body := make(map[string]any, 16)
+	body[ai.ModelParamName] = model
+	return &Operation{
+		req:  req,
+		body: body,
+	}, nil
 }
 
 // -----------------------------------------------------------------------------
 
 type Operation struct {
-	req *Request
+	req  *Request
+	body map[string]any
 }
 
-func (p Operation) Call(ctx context.Context, svc xai.Service, opts xai.OptionBuilder) (resp xai.OperationResponse, err error) {
+func (p *Operation) InputSchema() xai.InputSchema {
+	panic("todo")
+}
+
+func (p *Operation) Params() xai.Params {
+	return p
+}
+
+func (p *Operation) Set(name string, val any) xai.Params {
+	p.body[name] = val
+	return p
+}
+
+func (p *Operation) Call(ctx context.Context, svc xai.Service, opts xai.OptionBuilder) (resp xai.OperationResponse, err error) {
 	ret, err := p.req.Do(ctx, opts.(*HTTPOptions))
 	if err != nil {
 		return nil, err
 	}
-	_ = ret
+	defer ret.Body.Close()
+
+	var body map[string]any
+	dec := json.NewDecoder(ret.Body)
+	err = dec.Decode(&body)
+	if err != nil {
+		return nil, err
+	}
+
+	_ = body
 	panic("todo")
 }
 
